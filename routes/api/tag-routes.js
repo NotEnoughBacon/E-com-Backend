3237ebafcf63a83e4e4ba24136ad39761a641616
NotEnoughBacon <jboyk3@hotmail.com>
2023-03-08 10:45:59 -0700

    finished all routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,27 +3,78 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/tags', (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
-  
+  try { let tagData = await Tag.findAll({
+
+    include: [{model: Product}],
+  });
+
+  res.status(200).json(tagData);
+  } catch (err) {
+
+    res.status(500).json(err);
+  }
 });
 
-router.get('/tags:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try { let tagData = await Tag.findByPk(req.params.id, {
+
+    include: [{model: Product}],
+  });
+
+  res.status(200).json(tagData);
+  } catch (err) {
+
+    res.status(500).json(err);
+  }
 });
 
-router.post('/tags', (req, res) => {
+router.post('/', (req, res) => {
   // create a new tag
+  Tag.create(req.body).then((tagData) => {
+
+    res.status(200).json(tagData);
+  }).catch((err) => {
+
+    res.status(400).json(err);
+  })
 });
 
-router.put('/tags:id', (req, res) => {
+router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update(
+    {
+      tag_name: req.body.tag_name,
+    },
+    {
+      where: {
+        id: req.params.id
+      }
+    }
+  ).then((updated) => {
+
+    res.status(200).json(updated);
+  }).catch((err) => 
+
+    res.status(400).json(err));
 });
 
-router.delete('/tags:id', (req, res) => {
+router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy(
+    {
+      where: {
+        id: req.params.id
+      }
+    }
+  ).then((deleted) => {
+
+    res.status(200).json(deleted);
+  }).catch((err) => res.status(400).json(err))
 });
 
 module.exports = router;
